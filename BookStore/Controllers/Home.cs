18aeed1b7392b0e75176d2d77a4e3dd891021ff0
Akil Aktar <akil.aktar@fynxt.com>
2023-07-11 17:48:54 +0530

    tes1 git

---
+++ BookStore/Controllers/Home.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class Home : Controller
+    {
+        public string Index()
+        {
+            return "Akil";
+        }
+    }
+}
---
+++ BookStore/Program.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+//app.Use(async (context, next) =>
+//{
+//    await context.Response.WriteAsync("hellow my first middleware");
+//    await next();
+//});
+
+//app.Use(async (context, next) =>
+//{
+//    await context.Response.WriteAsync("Hello World From 2nd Middleware!");
+
+//    await next();
+//});
+
+//app.Run(async (context) =>
+//{
+//    await context.Response.WriteAsync("Hello World From 3rd Middleware");
+//});
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+//app.UseEndpoints(endpoint =>
+//{
+
+//    endpoint.Map("/akil", async context =>
+//    {
+//        await context.Response.WriteAsync("Hello Akil World");
+//    });
+//});
+
+app.UseEndpoints(endpoint =>
+{
+    endpoint.MapDefaultControllerRoute();
+});
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
